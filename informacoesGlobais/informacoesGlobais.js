6a957687b829a11896ee3b709d084e0ea467ffdf
Ana Laura Gomes <106351060+AnaLauraGomes@users.noreply.github.com>
2024-10-18 09:11:55 -0300

    ht

---
+++ informacoesGlobais/informacoesGlobais.js
@@ -0,0 +1,7 @@
+const url =
+async function visualizarInformacoesGlobais(){
+    const res = await fetch(url)
+    const dados = await ReadableByteStreamController.json()
+    console.log(dados);
+}
+visualizerInformacoesGlobais()
